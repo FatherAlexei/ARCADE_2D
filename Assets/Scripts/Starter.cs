d3d4206ac7489e8dc1eb3c69ce8f6a3b74e4c5e7
FatherAlexei <68125662+FatherAlexei@users.noreply.github.com>
2022-07-21 17:17:51 +0300

    Add files via upload

--- Assets/Scripts/Starter.cs
+++ Assets/Scripts/Starter.cs
@@ -6,21 +6,22 @@ namespace Arcade
 {
     public class Starter : MonoBehaviour
     {
-        [SerializeField] private SpriteAnimatorConfig _playerConfig;
-        [SerializeField] private int animSpeed = 15;
+        [SerializeField] private PlayerController _playerController;
         [SerializeField] private LevelObjectView _playerView;
-        [SerializeField] private SpriteAnimatorController _playerAnimator;
+        [SerializeField] private CannonAimController _aimController;
+        [SerializeField] private CannonView _cannonView;
+
         void Awake()
         {
-            _playerConfig = Resources.Load<SpriteAnimatorConfig>("PlayerAnimCfg");
-            _playerAnimator = new SpriteAnimatorController(_playerConfig);
-            _playerAnimator.StarAnimation(_playerView._spriteRenderer, AnimState.Run, true, animSpeed);
+            _playerController = new PlayerController(_playerView);
+            _aimController = new CannonAimController(_cannonView.muzzleTransform, _playerView._transform);
         }
 
         // Update is called once per frame
         void Update()
         {
-            _playerAnimator.Update();
+            _playerController.Update();
+            _aimController.Update();
         }
     }
 }
---
+++ Assets/Scripts/View/CannonView.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Arcade
+{
+    public class CannonView : MonoBehaviour
+    {
+        public Transform muzzleTransform;
+        public Transform emitter;
+        public List<LevelObjectView> bullets;
+    }
+}
